a794c94bbc34f069e719ca4ae975e6f2f319fb74
江﨑結音 <ezaki@ezaki-yune-no-MacBook-Air.local>
2020-10-08 14:19:48 +0900

    コンフリクト解消

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'articles#index'
 
-<<<<<<< HEAD
-  resources :articles, only: [:show, :new, :create,]
-=======
-  resources :articles, only: [:show, :new, :create,]
->>>>>>> delete-article
+  resources :articles, only: [:show, :new, :create, :edit, :update, :destroy]
+
 end
